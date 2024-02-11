ef3e79f1c98f2cbcbe25a1df5f7fa726eb32c0ed
Amin <medamineharwel@outlook.com>
2024-02-11 15:25:08 +0100

    refactored the infinite scroll data load logic

--- src/app/inifinte-scroll-matselect/inifinte-scroll-matselect.component.ts
+++ src/app/inifinte-scroll-matselect/inifinte-scroll-matselect.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { BehaviorSubject, map, Observable, of, scan, startWith } from 'rxjs';
+import { BehaviorSubject, map, Observable, of, scan, startWith, Subject, switchMap } from 'rxjs';
 
 @Component({
   templateUrl: './inifinte-scroll-matselect.component.html',
@@ -13,46 +13,38 @@ export class InifinteScrollMatselectComponent {
   total = 6000;
   data = Array.from({length: this.total}).map((_, i) => `Option ${i}`);
   limit = 10;
-  offset = 0;
-  options = new BehaviorSubject<string[]>([]);
-  options$: Observable<string[]>;
+
+  dataRequest = new Subject<{block: number, filter: string}>();
+  options$: Observable<string[]> = this.dataRequest.asObservable().pipe(
+    startWith({block: 0, filter: ''}),
+    switchMap(({block, filter}) => this.loadDataBlock(block, filter)),
+    scan((acc: any[], curr) => {
+      if (this.reset) return [...curr];
+      return [...acc, ...curr];
+    }, [])
+  );
 
   reset: boolean = false;
 
   myControl = new FormControl('');
-  optionsAuto: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]> = new Observable();
-
-  constructor() {
-      this.options$ = this.options.asObservable().pipe(
-          scan((acc: any[], curr) => {
-            if (this.reset) return [...curr];
-            return [...acc, ...curr];
-          }, [])
-      );
-  }
 
   ngOnInit() {
-    this.getBlock(0, '', false);
-  }
-
-  getNextBatch() {
-      const result = this.data.slice(this.offset, this.offset + this.limit);
-      this.options.next(result);
-      this.offset += this.limit;
   }
 
   getBlock(block: number, filter: string, reset: boolean) {
     this.reset = reset;
     let nextBatch: any[] = [];
-    nextBatch = (!!filter.trim() ? [...this.data.filter(element => element.includes(filter))] : [...this.data])
-      .slice(block * this.limit, block * this.limit + this.limit)
     console.log(`Block NO: ${block} | Filter: ${filter} | Reset: ${reset} | Result: `, nextBatch);
-    this.options.next(nextBatch);
+    this.dataRequest.next({block, filter});
   }
 
   onSelect($event: MatAutocompleteSelectedEvent) {
     console.log($event.option.value);
+  }
+
+  loadDataBlock(block: number, filter: string): Observable<string[]> {
+    return of((!!filter.trim() ? [...this.data.filter(element => element.includes(filter))] : [...this.data])
+      .slice(block * this.limit, block * this.limit + this.limit));
   }
 
 }
